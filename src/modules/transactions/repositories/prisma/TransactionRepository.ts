7dfeac45ca0d388945f3ce72f0aceed4f7f0bc3f
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2023-01-17 23:29:03 -0300

    fix: :bug: Fixing BETWEEN clause SQL (now has inclusive dates)

--- src/modules/transactions/repositories/prisma/TransactionRepository.ts
+++ src/modules/transactions/repositories/prisma/TransactionRepository.ts
@@ -10,7 +10,7 @@ class TransactionRepository implements ITransactionRepository {
 	} 
 
 	async findAllTransactionsByUserId({ userId , options : {  date = undefined,limit = undefined,offset = undefined} }: IFindAllTransactionsByUserIdParams) {
-		const handleData = date ? `AND date BETWEEN '${date.start_date}' AND '${date.end_date}' `: "";
+		const handleData = date ? `AND date BETWEEN '${date.start_date} 00:00:00' AND '${date.end_date} 23:59:59' `: "";
 		const handleLimit = limit ? ` LIMIT ${limit}` : "";
 		const handleOffset = limit ? ` OFFSET ${offset}` : "";
 		return await prisma.$queryRawUnsafe<ITransaction[]>(`SELECT * FROM transactions WHERE user_id = '${userId}' ${handleData} ${handleLimit} ${handleOffset}`);
